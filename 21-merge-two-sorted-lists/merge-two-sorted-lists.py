796f1b18c7552020659300fa3c189f005079f60d
Sanjit Pandey <166752012+sanjitvbp13@users.noreply.github.com>
2025-06-02 07:57:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (7.36%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,13 @@
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]"""
+        if not list1 or not list2:
+            return list1 if list1 else list2
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+        list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
